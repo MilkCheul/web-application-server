25660dc7566c49affa2ee8f9648975f8595530f7
MilkCheul <aktnftk1>
2017-05-28 22:36:50 +0900

    [요구사항 개발] - 진행중

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,18 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,15 +27,68 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream(); BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String line = "";
+            String httpRequest = "";
+
+            while(!"".equals(line = br.readLine())){
+                if(line == null){
+                    return;
+                }
+
+                log.info("http request string : " + line);
+
+                httpRequest += line;
+            }
+            br.readLine();
+
+            String paramString = br.readLine();
+
+            System.out.println(paramString);
+
+            String url = HttpRequestUtils.getUrl(httpRequest);
+
+            if("/user/create".equals(url)){
+                Map<String, String> model = HttpRequestUtils.parseQueryString(HttpRequestUtils.getParamString(httpRequest));
+
+                User user = (User)getModel(model, User.class);
+
+                System.out.println(user.toString());
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
+    }
+
+    private Object getModel(Map<String, String> model, Class clazz) throws Exception{
+        Constructor constructor = clazz.getConstructor();
+        Object object = constructor.newInstance();
+
+        Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            if("set".equals(method.getName().substring(0,3))){
+                String fieldName = method.getName().substring(3);
+
+                method.invoke(object, model.get(changeToLowerCaseForFirstString(fieldName)));
+            }
+        }
+
+        return object;
+    }
+
+    private String changeToLowerCaseForFirstString(String str){
+        String transString = str.substring(0, 1);
+        transString = transString.toLowerCase();
+        transString += str.substring(1);
+
+        return transString;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
